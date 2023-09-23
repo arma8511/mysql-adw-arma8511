826840c0290106f3f30e8343ecfa45a276876899
Armando Arevalo <arma8511@hotmail.com>
2023-09-23 17:05:33 +0000

    Cargue

--- scripts/consulta.sql
+++ scripts/consulta.sql
@@ -1,7 +1,32 @@
 use adw;
 
 -- Empleados
-select *
-from HumanResources_Employee
-limit 5
+select 
+    detail.SalesOrderDetailID,
+    sales.SalesOrderID,
+    sales.OrderDate,
+    sales.CustomerID,
+    sales.salesPersonID,
+    sales.TerritoryID,
+    product.Name as ProductName,
+    concat(person.FirstName, ' ', person.LastName) as CustomerName,
+    concat(employee.FirstName, ' ', employee.LastName) as SalesPersonName,
+    territory.name as TerritoryName
+from Sales_SalesOrderDetail as detail
+    join Sales_SalesOrderHeader as sales
+        on sales.SalesOrderID = detail.SalesOrderID
+    join Production_Product as product
+        on detail.ProductID = product.ProductID
+    join Sales_Customer as client  
+        on sales.CustomerID = client.CustomerID
+    left outer join Person_Person as person
+        on client.PersonID = person.BusinessEntityID
+    left outer join Sales_SalesPerson as salesperson
+        on sales.SalesPersonID = salesperson.BusinessEntityID
+    left outer join Person_Person as employee
+        on salesperson.BusinessEntityID = employee.BusinessEntityID
+    join Sales_SalesTerritory as territory
+        on sales.TerritoryID = territory.TerritoryID
+limit 3
 ;
+
